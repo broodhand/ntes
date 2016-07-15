044dbc04181390f684c51c598c94f4e6d54a5377
zhao cheng <3939789@qq.com>
2016-07-15 12:50:41 +0800

    config 0.1.1 Completely Done

--- logstash/config.py
+++ logstash/config.py
@@ -1,2 +1,25 @@
-rediscfg = dict(socket_timeout=3)
-logstashlist = 'logstash-list'
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 12:37:57 2016
+@author: Zhao Cheng
+__version__ = '0.1.1'
+To config logstash
+"""
+import logging; logging.basicConfig(level=logging.INFO)
+import config_default
+import base
+
+logstash = config_default.logstash
+
+try:
+    from . import config_override
+    base.merge(logstash, config_overrides.logstash)
+    logging.info('Get the config_override')
+except ImportError:
+    logging.info('config_override not exist')
+
+
+
+
+
+
---
+++ logstash/config_override.py
@@ -0,0 +1,11 @@
+"""
+Created on Wed Mar 23 12:37:57 2016
+@author: Zhao Cheng
+__version__ = '0.1.0'
+"""
+
+logstash = {
+    'redisAPI_config': {
+        'host': '127.0.0.1',
+    }
+}
