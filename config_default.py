f070a479a310d16a87be1fa88e8909036e05fd41
zhao cheng <3939789@qq.com>
2016-07-15 11:10:20 +0800

    config_default version 0.1.0 Completely Done

---
+++ config_default.py
@@ -0,0 +1,12 @@
+"""
+Created on Wed Mar 23 12:37:57 2016
+@author: Zhao Cheng
+__version__ = '0.1.0'
+"""
+
+logstash = {
+    'rediscfg': {
+        'socket_connect_timeout': 3
+    },
+    'logstashlist': 'logstash-list'
+}
