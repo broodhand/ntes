26bc76802c28e5c9fda37fc96a71a677f209b734
zhao cheng <3939789@qq.com>
2016-08-03 17:28:04 +0800

    ntesDS.url 0.1.0 Completely Done

--- ntesDS/url.py
+++ ntesDS/url.py
@@ -5,7 +5,6 @@ Created on Wed Mar 23 12:37:57 2016
 __version__ = '0.1.0'
 Tools for ntes
 """
-import logging; logging.basicConfig(level=logging.INFO)
 import base
 from collections import Iterator
 
@@ -38,5 +37,3 @@ def make_urls(codes, slices=1000):
             yield _make_url(*code_tuple, input_max=slices)
     else:
         raise TypeError('Must input a generator,list or tuple')
-
-
