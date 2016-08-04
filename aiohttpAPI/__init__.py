2676813ed37635db12fee114abf209cb12c248a1
zhao cheng <3939789@qq.com>
2016-08-04 10:21:30 +0800

    aiohttpAPI 0.4.1 Completely Done

--- aiohttpAPI/__init__.py
+++ aiohttpAPI/__init__.py
@@ -1,6 +1,6 @@
 from .client import (get_urls, get_url, Urls)
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 VERSION = tuple(map(int, __version__.split('.')))
 
 __all__ = ['get_urls', 'get_url', 'Urls']
