2f2c28e03b563a534e54e060d187fecb98629cb1
zhao cheng <3939789@qq.com>
2016-08-04 13:13:52 +0800

    ntesDS.stock 0.0.3 Completely Done

--- ntesDS/stock.py
+++ ntesDS/stock.py
@@ -2,8 +2,8 @@
 """
 Created on Wed Mar 23 12:37:57 2016
 @author: Zhao Cheng
-__version__ = '0.0.1'
-Code lib
+__version__ = '0.0.3'
+Stock lib
 """
 import logging; logging.basicConfig(level=logging.INFO)
 from .code import of_code_generator, sh_code_generator, sz_code_generator
@@ -18,73 +18,92 @@ generator = {
 
 
 class Base(object):
-    def __init__(self, codes_type, scheme='default', timeout=1, retry_session=3, semaphore=20, retry_failure=3):
+    def __init__(self, codes_type, scheme='default', timeout=1, retry_session=3, semaphore=20, retry_failure=True):
         global generator
         if codes_type in generator.keys():
-            self._generator = generator[codes_type]
+            self.__generator = generator[codes_type]
         else:
             raise CodeError('Please select OF,SZ or SH')
-        self._scheme = scheme
-        self._timeout = timeout
-        self._retry_session = retry_session
-        self._semaphore = semaphore
-        self._retry_failure = retry_failure
-        self._init = False
-        self._data_tuple = None
-        self._data = None
-        self._report = None
-        self._num = None
-        self._codes = None
-        self._content = None
+
+        self.kwargs = dict()
+        self.kwargs['codes_type'] = codes_type
+        self.kwargs['scheme'] = scheme
+        self.kwargs['timeout'] = timeout
+        self.kwargs['retry_session'] = retry_session
+        self.kwargs['semaphore'] = semaphore
+        self.kwargs['retry_failure'] = retry_failure
+
+        self.__init = False
+
+        self.__data_tuple = None
+        self.__data = None
+        self.__report = None
+        self.__num = None
+        self.__codes = None
+        self.__content = None
 
     def init(self):
-        if self._init:
+        if self.__init:
             raise CodeError('Already init')
 
-        self._data_tuple = get_data(self._generator(), scheme=self._scheme, timeout=self._timeout,
-                                    retry_session=self._retry_session, semaphore=self._semaphore,
-                                    retry_failure=self._retry_failure)
-        if self._data_tuple:
-            self._data = self._data_tuple[0]
-            self._report = self._data_tuple[1]
-            self._num = len(self._data)
-            self._codes = set(self._data.keys())
-            self._content = list(self._data.values())
+        self.__data_tuple = get_data(self.__generator(), scheme=self.kwargs['scheme'], timeout=self.kwargs['timeout'],
+                                     retry_session=self.kwargs['retry_session'], semaphore=self.kwargs['semaphore'],
+                                     retry_failure=self.kwargs['retry_failure'])
+        if self.__data_tuple:
+            self.__data = self.__data_tuple[0]
+            self.__report = self.__data_tuple[1]
+            self.__num = len(self.__data)
+            self.__codes = set(self.__data.keys())
+            self.__content = list(self.__data.values())
 
-        self._init = True
+        self.__init = True
+
+    def refresh(self):
+        if not self.__init:
+            raise CodeError('Need init first')
+
+        self.__data_tuple = get_data(list(self.__codes), scheme=self.kwargs['scheme'], timeout=self.kwargs['timeout'],
+                                     retry_session=self.kwargs['retry_session'], semaphore=self.kwargs['semaphore'],
+                                     retry_failure=self.kwargs['retry_failure'])
+        if self.__data_tuple:
+            self.__data = self.__data_tuple[0]
+            self.__report = self.__data_tuple[1]
+            self.__num = len(self.__data)
+            self.__codes = set(self.__data.keys())
+            self.__content = list(self.__data.values())
 
     @property
     def data(self):
-        if self._init:
-            return self._data
+        if self.__init:
+            return self.__data
         else:
             raise CodeError('Need init first')
 
     @property
     def report(self):
-        if self._init:
-            return self._report
+        if self.__init:
+            return self.__report
         else:
             raise CodeError('Need init first')
 
     @property
     def num(self):
-        if self._init:
-            return self._num
+        if self.__init:
+            return self.__num
         else:
             raise CodeError('Need init first')
 
     @property
     def codes(self):
-        if self._init:
-            return self._codes
+        if self.__init:
+            return self.__codes
         else:
             raise CodeError('Need init first')
 
     @property
     def content(self):
-        if self._init:
-            return self._content
+        if self.__init:
+            return self.__content
         else:
             raise CodeError('Need init first')
 
